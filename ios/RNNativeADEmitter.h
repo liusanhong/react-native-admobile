fa41dbe261d346a30d9343d2631c0d7066a3d5a6
zhoukai <zhoukai@bituniverse.org>
2022-06-18 13:39:19 +0800

    optimize: native 广告

---
+++ ios/RNNativeADEmitter.h
@@ -0,0 +1,16 @@
+//
+//  RNNativeADEmitter.h
+//  react-native-admobile
+//
+//  Created by zhoukai on 2022/6/15.
+//
+
+#import "RCTEventEmitter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNNativeADEmitter : RCTEventEmitter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RNNativeADEmitter.m
@@ -0,0 +1,12 @@
+//
+//  RNNativeADEmitter.m
+//  react-native-admobile
+//
+//  Created by zhoukai on 2022/6/15.
+//
+
+#import "RNNativeADEmitter.h"
+
+@implementation RNNativeADEmitter
+
+@end
