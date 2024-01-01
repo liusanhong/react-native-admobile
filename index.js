45ee573b6c942dc7af24aaf64c5370a94afc1dce
zhoukai <zhoukai@bituniverse.org>
2024-01-01 16:25:28 +0800

    feat: 修改导出

--- index.js
+++ index.js
@@ -4,40 +4,48 @@ import { NativeModules } from 'react-native';
 
 const { ReactNativeAdmobile } = NativeModules;
 
-export default ReactNativeAdmobile;
 
-export function initAd(appid) {
-  return ReactNativeAdmobile.initAd(appid);
-}
-/**
- * 开屏广告
- * @param adId
- * @param onSuccess
- * @param onError
- */
-export function splashAd(adId,onSuccess,onError) {
-  ReactNativeAdmobile.splashAd(adId,onSuccess,onError);
-}
-/**
- * 激励广告
- * @param adId
- * @param onSuccess
- * @param onError
- */
-export function rewardVodAd(adId,onSuccess,onError) {
-  ReactNativeAdmobile.rewardVodAd(adId,onSuccess,onError);
+
+ const ReactNativeAdmobileUtils ={
+
+    initAd(appid) {
+    return ReactNativeAdmobile.initAd(appid);
+  },
+  /**
+   * 开屏广告
+   * @param adId
+   * @param onSuccess
+   * @param onError
+   */
+  splashAd(adId,onSuccess,onError) {
+    ReactNativeAdmobile.splashAd(adId,onSuccess,onError);
+  },
+  /**
+   * 激励广告
+   * @param adId
+   * @param onSuccess
+   * @param onError
+   */
+  rewardVodAd(adId,onSuccess,onError) {
+    ReactNativeAdmobile.rewardVodAd(adId,onSuccess,onError);
+  },
+
+  /**
+   * 插屏广告
+   * @param adId
+   * @param onSuccess
+   * @param onError
+   */
+  intertitialAd(adId,onSuccess,onError) {
+    ReactNativeAdmobile.intertitialAd(adId,onSuccess,onError);
+  },
+
+  setPersonalizedAdEnabled(personalizedAdEnabled=false) {
+    ReactNativeAdmobile.setPersonalizedAdEnabled(personalizedAdEnabled);
+  }
+
 }
 
-/**
- * 插屏广告
- * @param adId
- * @param onSuccess
- * @param onError
- */
-export function intertitialAd(adId,onSuccess,onError) {
-  ReactNativeAdmobile.intertitialAd(adId,onSuccess,onError);
-}
+export default ReactNativeAdmobileUtils;
 
-export function setPersonalizedAdEnabled(personalizedAdEnabled=false) {
-  ReactNativeAdmobile.setPersonalizedAdEnabled(personalizedAdEnabled);
-}
+
