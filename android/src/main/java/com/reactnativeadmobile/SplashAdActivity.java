c69579dd234d9513fc64d3800c473312962fd60c
liuqiang <liuqiang@cfhy.com>
2024-05-21 09:23:19 +0800

    fix:  修改android的启动问题

--- android/src/main/java/com/reactnativeadmobile/SplashAdActivity.java
+++ android/src/main/java/com/reactnativeadmobile/SplashAdActivity.java
@@ -2,11 +2,16 @@ package com.reactnativeadmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
-//import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
 
 import cn.admobiletop.adsuyi.ad.ADSuyiSplashAd;
 import cn.admobiletop.adsuyi.ad.data.ADSuyiAdInfo;
@@ -14,6 +19,7 @@ import cn.admobiletop.adsuyi.ad.entity.ADSuyiAdSize;
 import cn.admobiletop.adsuyi.ad.entity.ADSuyiExtraParams;
 import cn.admobiletop.adsuyi.ad.error.ADSuyiError;
 import cn.admobiletop.adsuyi.ad.listener.ADSuyiSplashAdListener;
+import android.view.WindowManager;
 
 public class SplashAdActivity extends AppCompatActivity {
 
@@ -33,30 +39,34 @@ public class SplashAdActivity extends AppCompatActivity {
         // 创建开屏广告实例，第一个参数可以是Activity或Fragment，第二个参数是广告容器
         adSuyiSplashAd = new ADSuyiSplashAd(this, contentView);
 
-// 底部logo容器高度，请根据实际情况进行计算
-//        TODO
-        int logoHeight = 0;
-// 屏幕宽度px
-        int widthPixels = getResources().getDisplayMetrics().widthPixels;
-// 屏幕高度px
-        int heightPixels = getResources().getDisplayMetrics().heightPixels;
-// 创建额外参数实例
+        // 屏幕宽度px
+        float widthPixels = getResources().getDisplayMetrics().widthPixels;
+        // 屏幕高度px
+        float heightPixels = getResources().getDisplayMetrics().heightPixels;
+
+        // 原始屏幕密度
+        final float originalDensity = getRealScreenDensity(this);
+        // 当前屏幕密度
+        final float density = getResources().getDisplayMetrics().density;
+
+        // 当屏幕密度变大时。宽高也要根据扩大倍率重新计算
+        if (density > originalDensity) {
+            widthPixels = widthPixels * (density / originalDensity);
+            heightPixels = heightPixels * (density / originalDensity);
+        }
+
+        // 创建额外参数实例
         ADSuyiExtraParams extraParams = new ADSuyiExtraParams.Builder()
                 // 设置整个广告视图预期宽高(目前仅头条平台需要，没有接入头条可不设置)，单位为px，如果不设置头条开屏广告视图将会以9 : 16的比例进行填充，小屏幕手机可能会出现素材被压缩的情况
-                .adSize(new ADSuyiAdSize(widthPixels, heightPixels - logoHeight))
+                .adSize(new ADSuyiAdSize((int)widthPixels, (int)heightPixels))
                 .build();
-// 如果开屏容器不是全屏可以设置额外参数
+        // 如果开屏容器不是全屏可以设置额外参数
         adSuyiSplashAd.setLocalExtraParams(extraParams);
 
-// 设置是否是沉浸式，如果为true，跳过按钮距离顶部的高度会加上状态栏高度
+        // 设置是否是沉浸式，如果为true，跳过按钮距离顶部的高度会加上状态栏高度
         adSuyiSplashAd.setImmersive(false);
 
-// 设置自定义跳过按钮和倒计时时长（非必传，倒计时时长范围[3000,5000]建议不要传入倒计时时长） 目前不支持inmobi, ksad, oneway, ifly平台自定义跳过按钮
-// 注意不要隐藏跳过按钮，可以在布局中将跳过按钮alpha设置为0，在onAdReceive回调中将alpha设置为1
-//        TODO
-//        adSuyiSplashAd.setSkipView(skipView, 5000);
-
-// 设置开屏广告监听
+        // 设置开屏广告监听
         adSuyiSplashAd.setListener(new ADSuyiSplashAdListener() {
 
             @Override
@@ -88,13 +98,11 @@ public class SplashAdActivity extends AppCompatActivity {
             @Override
             public void onAdClick(ADSuyiAdInfo adSuyiAdInfo) {
                 Log.e(TAG, "广告点击回调，有点击回调不一定是有效点击，如网络等情况导致上报失败");
-//                 AdCallbackUtils.doSplashSuccessCallback();
             }
 
             @Override
             public void onAdClose(ADSuyiAdInfo adSuyiAdInfo) {
                 Log.e(TAG, "广告关闭回调，需要在此进行页面跳转");
-//                jumpMain();
                 mContext.finish();
             }
 
@@ -103,15 +111,28 @@ public class SplashAdActivity extends AppCompatActivity {
                 if (adSuyiError != null) {
                     String failedJson = adSuyiError.toString();
                     Log.e(TAG, "onAdFailed----->" + failedJson);
-//                     AdCallbackUtils.doSplashErrorCallback();
 
                 }
-//                jumpMain();
                 mContext.finish();
             }
         });
 
-//        adSuyiSplashAd.loadAd("377b03ea4dff47bda1");
         adSuyiSplashAd.loadAd(mAdId);
+    }
+
+    public static float getRealScreenDensity(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            Display display = windowManager.getDefaultDisplay();
+            DisplayMetrics realMetrics = new DisplayMetrics();
+            try {
+                Method getRealMetrics = Display.class.getMethod("getRealMetrics", DisplayMetrics.class);
+                getRealMetrics.invoke(display, realMetrics);
+                return realMetrics.density;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return -1f; // 返回-1表示获取失败
     }
 }
