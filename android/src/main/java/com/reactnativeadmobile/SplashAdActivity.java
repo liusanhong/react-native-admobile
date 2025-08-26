8e4de0793ac297dd5b50f0c208cb6f4190e89d04
matian <1165099390@qq.com>
2025-08-26 09:53:06 +0800

    优化代码

--- android/src/main/java/com/reactnativeadmobile/SplashAdActivity.java
+++ android/src/main/java/com/reactnativeadmobile/SplashAdActivity.java
@@ -65,20 +65,25 @@ public class SplashAdActivity extends AppCompatActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
         // 沉浸式显示
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | // 启用沉浸模式，避免系统栏意外出现
-                            View.SYSTEM_UI_FLAG_FULLSCREEN |     // 隐藏状态栏
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |// 隐藏导航栏
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |  // 保持布局稳定
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | // 保证全屏显示
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // 保证全屏时隐藏导航栏
-            );
-            List<Rect> exclusionRects = new ArrayList<>();
-            Rect rect = new Rect(0,0, (int) widthPixels, (int) heightPixels);
-            exclusionRects.add(rect);
-            decorView.setSystemGestureExclusionRects(exclusionRects);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            try {
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | // 启用沉浸模式，避免系统栏意外出现
+                                View.SYSTEM_UI_FLAG_FULLSCREEN |     // 隐藏状态栏
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |// 隐藏导航栏
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |  // 保持布局稳定
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | // 保证全屏显示
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // 保证全屏时隐藏导航栏
+                );
+                List<Rect> exclusionRects = new ArrayList<>();
+                Rect rect = new Rect(0,0, (int) widthPixels, (int) heightPixels);
+                exclusionRects.add(rect);
+                Method method = View.class.getMethod("setSystemGestureExclusionRects", List.class);
+                method.invoke(decorView, exclusionRects);
+            } catch (Exception e) {
+
+            }
         }
 
         // 原始屏幕密度
